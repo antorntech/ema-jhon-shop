047d5ad2b9ff14a2747ed41b5de319cf31a22794
antor10655gh <creativefreelancerantor@gmail.com>
2022-03-23 19:41:37 +0600

    add change selectedProduct to product

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -27,11 +27,22 @@ const Shop = () => {
         setCart(savedCart);
     },[products]);
 
-    const addToCart=(product)=>{
-        console.log(product);
-        const newCart = [...cart, product];
+    const addToCart=(selectedProduct)=>{
+        console.log(selectedProduct);
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+        
         setCart(newCart);
-        addToDb(product.id);
+        addToDb(selectedProduct.id);
     }
 
     return (
